35f8277e249dfe388c66fd5bf0615b824a97a867
Julien Ponge <jponge@redhat.com>
2023-07-20 11:59:36 +0200

    Upgrade Mutiny version to 2.3.1

--- src/main/java/_06_threading/_04_Threading_Blocking.java
+++ src/main/java/_06_threading/_04_Threading_Blocking.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.0.0
+//DEPS io.smallrye.reactive:mutiny:2.3.1
 package _06_threading;
 
 import io.smallrye.mutiny.Multi;
--- src/main/java/_07_misc/_03_Context.java
+++ src/main/java/_07_misc/_03_Context.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.smallrye.reactive:mutiny:2.0.0
+//DEPS io.smallrye.reactive:mutiny:2.3.1
 package _07_misc;
 
 import io.smallrye.mutiny.Context;
